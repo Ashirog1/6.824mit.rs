7d20bbaf6454614dae9bbd114e98e8fcb4f00fa6
Ashirog1 <nguyennhatquoc1012@gmail.com>
2026-06-07 21:12:18 +0700

    sync

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -17,11 +17,48 @@
 // TODO: Implement Network, ClientEnd, Server, Service types
 // Reference: go-version/src/labrpc/labrpc.go
 
-use tokio::sync::oneshot;
+use std::sync::Arc;
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use tokio::sync::{Mutex, oneshot};
 
 struct RawCall {
     server_name: String,
     method: String,
     args: Vec<u8>,
-    reply_tx: oneshot::Sender<Option<Vec<u8>>>;
+    reply_tx: oneshot::Sender<Option<Vec<u8>>>,
+}
+
+pub trait Service: Send + Sync {
+    fn dispatch(&self, method: &str, args: &[u8]) -> Vec<u8>;
+}
+
+pub struct NetworkInner {
+    pub reliable: bool,
+    pub servers: HashMap<String, Arc<dyn Service>>,
+}
+pub type Network = Arc<Mutex<NetworkInner>>;
+
+impl ClientEnd {
+    pub fn call<Req, Rep>(&self, method: &str, args: &Req) -> Option<Rep>
+    where
+        Req: Serialize,
+        Rep: for<'de> Deserialize<'de>, 
+    {
+        let net = self.network.lock().unwrap();
+        if !net.reliable && rand::random::<f64>() < 0.1 {
+            return None;
+        }
+        let server = net.servers.get(&self.server_name)?.clone();
+        let raw_args = bincode::seralize(args).unwarp();
+        drop(net);
+        
+        let raw_reply = server.dispatch(method, &raw_args);
+        
+        //unreliable
+        {
+            let net = self.network.lock().unwrap();
+        }
+    }
 }
